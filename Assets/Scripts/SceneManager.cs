3ff36206a75e95c718203e1faf25dc99a8a6d094
ErdemKaya <kayaerdem936@gmail.com>
2023-05-07 17:33:13 +0300

    add scene management

--- Assets/Scripts/SceneManager.cs
+++ Assets/Scripts/SceneManager.cs
@@ -2,23 +2,20 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneManager : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.transform.CompareTag("Level1Finish"))
+        if (other.transform.CompareTag("LevelFinish"))
         {
-            
+            LoadScene();
         }
-        if (other.transform.CompareTag("Level2Finish"))
-        {
-            
-        }
-        if (other.transform.CompareTag("Level3Finish"))
-        {
-            
-        }
-        
+    }
+
+    public void LoadScene()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().buildIndex +1);
     }
 }
